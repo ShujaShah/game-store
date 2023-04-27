9160449cf16d2f8d7cb17547ff3e70d2d9200f39
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-28 00:28:19 +0530

    added the chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
