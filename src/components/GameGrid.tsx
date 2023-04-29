29f318d5018c0f7fb5bcc0524ecdcb2e5c2cbd33
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-29 18:29:45 +0530

    fixed the large screen grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = () => {
     <>
       
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" spacing={10}>
       {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key="skeleton" />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
