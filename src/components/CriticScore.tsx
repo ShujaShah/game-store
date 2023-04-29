ddcfcf1230ffee8d7d281df7951f1d734c694975
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-29 16:41:46 +0530

    added the critic score in badge

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : "yellow";
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="5px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
