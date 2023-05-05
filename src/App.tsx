b58ecadc386f06c6f65a3311eadac6ab6d7b7085
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-06 00:52:40 +0530

    updated some packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
+import  { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
+
+//test commit
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
