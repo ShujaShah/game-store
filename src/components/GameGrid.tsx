6f3c5c4f7d76c18c8c722f4ccae54f6f611ffbe9
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-06 22:14:15 +0530

    adde the total of 8 skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
 
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   if (error) return <Text>{error}</Text>;
   return (
