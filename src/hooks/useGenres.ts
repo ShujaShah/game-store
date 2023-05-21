646f517a34baff3ffb19563cb22d7243e597b021
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-21 14:30:15 +0530

    fixed the bug in genre

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,20 +2,21 @@
 
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import APIClient from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 
-const apiClient = new APIClient<Genre>('/genres ')
-export interface Genre{
-    id: number;
-    name: string;
-    image_background: string;
+const apiClient = new APIClient<Genre>("/genres ");
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
 }
 //const useGenres =()=>useData<Genre>('/genres')
-const useGenres =()=>useQuery({
-    queryKey: ['genres'],
-    queryFn: apiClient.getAll ,
+const useGenres = () =>
+  useQuery<FetchResponse<Genre>>({
+    queryKey: ["genres"],
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: {count: genres.length, results: genres} // data inserted to the cache
-})
+    initialData: { count: genres.length, results: genres, next: null }, // data inserted to the cache
+  });
 
 export default useGenres;
