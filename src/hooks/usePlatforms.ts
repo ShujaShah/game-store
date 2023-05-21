c8ae9beed973a1486d78754e82356768473097fb
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-21 12:48:10 +0530

    added the cache for the platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,19 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import useData, { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
-interface Platform {
+export interface Platform {
     id: number;
     name: string;
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => useQuery<FetchResponse<Platform>, Error>({
+    queryKey: ["platform"],
+    queryFn: ()=> apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res=> res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    //initialData: { count: platforms.results.length,  results: platforms } 
+})
 
 export default usePlatforms;
