957cca3a8decd31cd1116c3206d997f81bae8ae3
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-27 23:19:06 +0530

    fixed the navbar issues - logo link and search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,20 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
-      <SearchInput  />
-      <ColorModeSwitch />
-    </HStack>
+    <>
+      <HStack padding="10px">
+        <Link to="/">
+          {" "}
+          <Image src={logo} boxSize="60px" objectFit="cover" />
+        </Link>
+        <SearchInput />
+        <ColorModeSwitch />
+      </HStack>
+    </>
   );
 };
 
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,12 +1,10 @@
-import { Grid, GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
-import GameTrailer from "../components/GameTrailer";
 import GameScreenshots from "../components/GameScreenshots";
+import GameTrailer from "../components/GameTrailer";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
