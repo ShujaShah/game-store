cb4e836d86776b9eeefe3f1202866f712c3c9a0f
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-27 22:41:09 +0530

    added the attributes of single game

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
@@ -6,6 +8,8 @@ export interface Game {
   slug: string;
   description_raw: string;
   background_image: string;
+  genres: Genre[];
+  publishers: Publisher[];
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -15,6 +18,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
